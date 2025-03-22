4d7ff7e13b687420b909e6d75b014fae401bac1f
BombasticFantastik <artemilusenko2@gmail.com>
2025-03-22 13:10:10 +0700

    Теперь созданные фотографии имеют текст в случайных местах

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 from random import randint
 
 
@@ -10,26 +12,27 @@ def create_images():
     alphabet='abcdefgh'
     back_img=Image.open('white.png')
     
-    max_widh,max_height=back_img.size
     
 
     word='aaa'
 
+    max_width,max_height=back_img.size
+
     selected_font=ImageFont.truetype('Comic Sans MS/Comic Sans MS.ttf',65)
     #font=ImageFont.load("arial.pil")
-
+    
     for i1 in range(len(alphabet)):
         for i2 in range(len(alphabet)):
             for i3 in range(len(alphabet)):
                 word=alphabet[i1]+alphabet[i2]+alphabet[i3]
-                img=back_img.copy()
-                img_draw=ImageDraw.Draw(img)
 
-                img_size=(randint(0,max_widh-200),randint(0,max_height-200))
-
-                img_draw.text(img_size,word,font=selected_font,fill=(255,0,0))
-                img.save(f'images/white_with_text{i1}{i2}{i3}.png')
-                print(word)
+                print(randint(0,max_width),randint(0,max_height))
+                
+                #img=back_img.copy()
+                #img_draw=ImageDraw.Draw(img)
+                #img_draw.text((28,28),word,font=selected_font,fill=(255,0,0))
+                #img.save(f'images/white_with_text{i1}{i2}{i3}.png')
+                #print(word)
 
 
 create_images()
