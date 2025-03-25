c2ffa7894dcffe2b98aacb92986a0c9be6e95e40
BombasticFantastik <artemilusenko2@gmail.com>
2025-03-25 23:42:15 +0700

    добавил train loop, понял что для обучения надо сохранять не обрезанные изображения, а координаты обрезки

--- Loop.py
+++ Loop.py
@@ -1,3 +1,17 @@
+from tqdm import tqdm
+
+
 def train_loop(model,optimizer,loss_func,dataloader):
-    for batch in loss_func:
+    for batch in (pbar:=tqdm(loss_func)):
         optimizer.zero_grad()
+
+        pred=model(batch['data'])
+        loss=loss_func(pred,batch['label'])
+        loss_item=loss.item()
+        loss.backward()
+
+        optimizer.step()
+
+        pbar.set_description(f'Loss: {loss_item}')
+
+        
